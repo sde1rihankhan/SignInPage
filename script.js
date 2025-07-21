faf25b6c14b075c72ec64f3d73dd189611f3e410
sde1rihankhan <sde1rihankhan@gmail.com>
2025-07-21 15:30:55 +0530

    JS file add

---
+++ script.js
@@ -0,0 +1,18 @@
+let nextPage = document.querySelector(".contant8")
+let inputs = document.querySelectorAll(".contant2")
+
+
+nextPage.addEventListener("click",()=>{
+    let value = []
+
+    inputs.forEach(input =>{
+        value.push(input.value)
+    })
+    localStorage.setItem("userdata", JSON.stringify(value))
+    console.log(value);
+    
+})
+
+function newPage(){
+    window.location.href = "http://127.0.0.1:5502/Musicart/home%20page/index.html"
+}
