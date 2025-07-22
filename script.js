7ad98d08500fa865c98346e5883c9297d59e9351
sde1rihankhan <sde1rihankhan@gmail.com>
2025-07-22 16:44:12 +0530

    HTML & CSS Changes

--- script.js
+++ script.js
@@ -4,15 +4,36 @@ let inputs = document.querySelectorAll(".contant2")
 
 nextPage.addEventListener("click",()=>{
     let value = []
+    let isValue = false
 
     inputs.forEach(input =>{
         value.push(input.value)
+        if(input.value ===""){
+            isValue= true
+            input.style.border = "2px solid red"
+        }
     })
+
+    inputs.forEach(input =>{
+        input.addEventListener("click",()=>{
+        input.style = "none"
+        })
+    })
+
+    if(isValue){
+        alert("Please fill inputs!")
+        return
+    }
+
     localStorage.setItem("userdata", JSON.stringify(value))
     console.log(value);
-    
+
+    window.location.href = "http://127.0.0.1:5502/Musicart/homePage/index.html"
+    inputs.forEach(input =>{
+        input.value = ""
+    })
 })
 
-function newPage(){
-    window.location.href = "http://127.0.0.1:5502/Musicart/home%20page/index.html"
-}
+// function newPage(){
+//     window.location.href = "http://127.0.0.1:5502/Musicart/homePage/index.html"
+// }
